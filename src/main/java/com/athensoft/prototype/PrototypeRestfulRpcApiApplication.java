1aa9848842bdfd7bd5e2c04ec6922b8c05829c6f
YiyuanShang <yiyuan.shang@mail.mcgill.ca>
2021-07-05 18:24:32 -0400

    init repo

---
+++ src/main/java/com/athensoft/prototype/PrototypeRestfulRpcApiApplication.java
@@ -0,0 +1,13 @@
+package com.athensoft.prototype;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrototypeRestfulRpcApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrototypeRestfulRpcApiApplication.class, args);
+	}
+
+}
